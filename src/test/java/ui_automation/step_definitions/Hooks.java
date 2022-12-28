187dec52b381f861f5d924a0825d4b23660d8a92
MvElvin <mr.elvinmustafayev@gmail.com>
2022-12-28 14:20:27 -0500

    Hooks comment

--- src/test/java/ui_automation/step_definitions/Hooks.java
+++ src/test/java/ui_automation/step_definitions/Hooks.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class Hooks {
     public WebDriver driver=null;
 
+    // before each scenario
     @Before
     public void setUp(){
         driver= BrowserFactory.createInstance();
@@ -25,6 +26,7 @@ public class Hooks {
         driver.manage().window().maximize();
     }
 
+    // before each scenario
     @After
     public void tearDown(Scenario scenario) {
         if (scenario.isFailed()) {
